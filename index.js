73dd2f37aad6742be227fca316c845c05aa836de
deveshaha <devesh_hanumante@outlook.com>
2023-04-13 15:24:25 +0200

    added comments

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict'
 
+// We instance the objects "App" and "BrowserWindow"
 const { app, BrowserWindow } = require('electron')
 
-
+//We print a message in the console before quitting the app
 app.on('before-quit', () => {
     console.log("Saliendo de la app...")
 })
 
+// This method will do what is inside only when the app is ready
 app.on('ready', () => {
     console.log("Abriendo la app..")
+    //Here we are creating a new Browser window
     let win = new BrowserWindow()
 
+    //This methond will detect when the window is closed!
     win.on('closed', () => {
-        win = null;
+        win = null; //We make it null to free memory!
         app.quit()
     })
 })
